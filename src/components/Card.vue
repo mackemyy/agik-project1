fa5ccfb56f474bd79d408752d75518f05a08e536
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-01-23 17:58:26 +0800

    Create Card Component

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import Header from './Header.vue';
+import Todo from './Todo.vue';
+
+
+    export default {
+        name: 'CardVue',
+        components: {
+            Header,
+            Todo
+        }
+    }
+
+</script>
+
+<template>
+    <div class=" w-[28rem] h-[45rem] rounded-2xl border-4 border-t-purple-600 border-r-pink-600 border-b-pink-600 border-l-indigo-600  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-md shadow-white font-sans">
+        <Header/>
+        <Todo/>
+    </div>
+
+</template>
